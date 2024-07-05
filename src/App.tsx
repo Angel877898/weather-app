8fd5d0738f37964c4dded23a9239028a129f4a84
Angel877898 <50708206+Angel877898@users.noreply.github.com>
2024-07-05 11:02:04 -0600

    feat:UpdatingUserSlices

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,44 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState, AppDispatch } from './store';
+import { fetchWeatherStart, fetchWeatherSuccess, fetchWeatherFailure } from './store/weatherSlice';
+import { loadUsernameFromStorage, setUsername } from './store/userSlice';
+import { getWeather, getCoordinates } from './api/weatherService';
+import SearchInput from './components/SearchInput';
+import WeatherCard from './components/WeatherCard';
+import { Container, Typography, CircularProgress } from '@mui/material';
 
-function App() {
+const App: React.FC = () => {
+  const dispatch: AppDispatch = useDispatch();
+  const weather = useSelector((state: RootState) => state.weather);
+  const user = useSelector((state: RootState) => state.user);
+
+  useEffect(() => {
+    dispatch(loadUsernameFromStorage());
+  }, [dispatch]);
+
+  const handleSearch = async (city: string) => {
+    dispatch(fetchWeatherStart());
+    try {
+      const { lat, lon } = await getCoordinates(city);
+      const data = await getWeather(lat, lon);
+      data.city_name = city;
+      dispatch(fetchWeatherSuccess(data));
+    } catch (error) {
+      dispatch(fetchWeatherFailure('Error fetching weather data'));
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Typography variant="h4">Weather App</Typography>
+      <Typography variant="h6">Hello, {user.username || 'Guest'}!</Typography>
+      <SearchInput onSearch={handleSearch} />
+      {weather.loading && <CircularProgress />}
+      {weather.error && <Typography color="error">{weather.error}</Typography>}
+      {weather.data && <WeatherCard data={weather.data} />}
+    </Container>
   );
-}
+};
 
 export default App;
--- src/store/userSlice.ts
+++ src/store/userSlice.ts
@@ -1,29 +1,34 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface UserState {
-  username: string;
+  username: string | null;
 }
 
 const initialState: UserState = {
-  username: '',
+  username: null,
 };
 
 const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
-    setUsername(state, action: PayloadAction<string>) {
+    setUsername: (state, action: PayloadAction<string>) => {
       state.username = action.payload;
       localStorage.setItem('username', action.payload);
     },
-    loadUsernameFromStorage(state) {
+    loadUsernameFromStorage: (state) => {
       const username = localStorage.getItem('username');
       if (username) {
         state.username = username;
       }
     },
+    logout: (state) => {
+      state.username = null;
+      localStorage.removeItem('username');
+    },
   },
 });
 
-export const { setUsername, loadUsernameFromStorage } = userSlice.actions;
+export const { setUsername, loadUsernameFromStorage, logout } = userSlice.actions;
+
 export default userSlice.reducer;
