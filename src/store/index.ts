ea45a6591afb2297bbf8cb493c7745d817a3c87b
Angel877898 <50708206+Angel877898@users.noreply.github.com>
2024-07-05 10:31:10 -0600

    feat: configure Redux with weather and user slices

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-
+import weatherReducer from './weatherSlice';
+import userReducer from './userSlice';
 
 const store = configureStore({
   reducer: {
-
+    weather: weatherReducer,
+    user: userReducer,
   },
 });
 
